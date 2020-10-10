09bd11382428fcddcdb01cbef6673191c8df95e9
Ahmed Fathy <ahmedfatmahmoud@gmail.com>
2020-10-10 18:21:39 +0200

    clean up some folders

--- wuzzuf scrapper/scrapper_test.py
+++ wuzzuf scrapper/scrapper_test.py
@@ -6,8 +6,9 @@ link="https://wuzzuf.net/search/jobs?q=it&a=hpb"
 r=requests.get(link)
 soup=BeautifulSoup(r.text, "html.parser")
 
-f = open("demo.html","w")
-f.write(r.text)
-f.close()
+
 s=soup.find("div", {"class": "result-wrp row"})
 print(s.contents)
+f = open("demo.html","w")
+f.write(s.text)
+f.close()
