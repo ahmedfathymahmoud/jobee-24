4f2ac55e0664bb56bd6939220a88ef2ac5399446
Ahmed Fathy <ahmedfatmahmoud@gmail.com>
2020-10-10 23:10:20 +0200

    job count

--- wuzzuf scrapper/scrapper_test.py
+++ wuzzuf scrapper/scrapper_test.py
@@ -2,13 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 
 import json
+
 link="https://wuzzuf.net/search/jobs?q=it&a=hpb"
 r=requests.get(link)
 soup=BeautifulSoup(r.text, "html.parser")
 
+ss=soup.find_all("div", {"class": "result-wrp row"})
+jobCount=soup.find("span", {"class": "search-jobs-count"}).text
+print("Job Count:",jobCount,"\n")
+for s in ss:
+    title=s.div.contents[5].h2.text
+    title=title.replace("\n", "")
+    print("title: ",title)
 
-s=soup.find("div", {"class": "result-wrp row"})
-print(s.contents)
-f = open("demo.html","w")
-f.write(s.text)
-f.close()
+    company=s.div.contents[7].contents[1].text
+    company=company.replace("\n", "").replace("  ", "")
+    print("company: ", company)
+
+    location=s.div.contents[7].contents[5].text
+    location=location.replace("\n", "").replace("  ", "")
+    print("location: ",location)
+
+    exp=s.div.contents[9].contents[3].text
+    exp=exp.replace("\n","").replace("  ","")
+    print("exp: ",exp)
+
+    tags=s.div.contents[9].contents[5].text
+    tags=tags.replace("\n", "").replace("  ", "")
+    print("tags: ",tags)
+
+    jobType=s.div.contents[9].contents[1].text
+    jobType=jobType.replace("\n", "").replace("  ", "")
+    print("job Type: ",jobType)
+
+    time=s.div.find("time").attrs['title']
+    time=time.replace("\n", "").replace("  ", "")
+    print("time: ",time)
+
+    print("\n\n\n")
+
