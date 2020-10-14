e02349ed0ee7a6663dc8286b7c8bea1323cdccdd
Ahmed Fathy <ahmedfatmahmoud@gmail.com>
2020-10-14 04:11:43 +0200

    refactoring extractor

---
+++ wuzzuf scrapper/jobExtractor.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import re
+
+class Job:
+    def __init__(self):  # S is a soup object
+        self.title = ""
+        self.company = ""
+        self.location = ""
+        self.exp = ""
+        self.tags = ""
+        self.jobType = ""
+        self.postTime = ""
+
+
+
+    def getJobDynamic(self,s):
+        def prep(s):
+            return s.replace("\n", "").strip()
+        self.title = prep(s.div.find('h2',{"class":"job-title"}).text)
+        self.company = prep(s.div.find('span',{"class":"company-name"}).text)
+        self.location = prep(s.div.find('span',{"class":"location-mobile"}).text)
+        self.details=prep(s.div.find('div', {"class": "job-details"}).text)
+        self.tags ,self.jobType ,self.expLevel,self.exp=self.getTagsTime(self.details)
+        self.jobLink=s.div.find('a',{"class":"mobile-job-link"}).attrs['href']
+        self.postTime = s.div.find("time").attrs['title']
+    def getJobStatic(self,s):
+        self.title = s.div.contents[5].h2.text
+        self.company = s.div.contents[7].contents[1].text
+        self.location = s.div.contents[7].contents[5].text
+        self.exp = s.div.contents[9].contents[3].text
+        self.tags = s.div.contents[9].contents[5].text
+        self.jobType = s.div.contents[9].contents[1].text
+        self.postTime = s.div.find("time").attrs['title']
+
+    def getTagsTime(self,detailsElement):
+        detailsElement = detailsElement.replace("\n", "").replace("  ", "").split('·')
+        detailsElement=list(map(str.strip, detailsElement))
+        p = detailsElement.pop().split('-')
+        time=p.pop()
+        detailsElement.extend(p)
+        detailsElement=set(detailsElement)
+        jobTypeWordList={'Full Time','Part Time','Work From Home','Freelance / Project '}
+        ExpWordList={'Entry Level','Manager','Experienced','Senior Management'}
+        jobType=jobTypeWordList & detailsElement
+        ExpLevel=detailsElement & ExpWordList
+        exp=list(filter(lambda x: 'Yrs of Exp' in x, detailsElement))[0]
+        detailsElement=detailsElement-(jobType|ExpLevel|{exp})
+        return detailsElement,jobType,ExpLevel,exp
+
+
--- wuzzuf scrapper/scrapper_test.py
+++ wuzzuf scrapper/scrapper_test.py
@@ -1,43 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-import json
+from jobExtractor import Job
+link = "https://wuzzuf.net/search/jobs?q=it&a=hpb"
+filteredLink = "https://wuzzuf.net/a/IT-Software-Development-Jobs-in-Egypt"
+r = requests.get(filteredLink)
+soup = BeautifulSoup(r.text, "html.parser")
 
-link="https://wuzzuf.net/search/jobs?q=it&a=hpb"
-r=requests.get(link)
-soup=BeautifulSoup(r.text, "html.parser")
+ss = soup.find_all("div", {"class": "result-wrp row"})
+jobCount = soup.find("span", {"class": "search-jobs-count"}).text
+print("Job Count:", jobCount, "\n")
 
-ss=soup.find_all("div", {"class": "result-wrp row"})
-jobCount=soup.find("span", {"class": "search-jobs-count"}).text
-print("Job Count:",jobCount,"\n")
+job=Job()
+
 for s in ss:
-    title=s.div.contents[5].h2.text
-    title=title.replace("\n", "")
-    print("title: ",title)
-
-    company=s.div.contents[7].contents[1].text
-    company=company.replace("\n", "").replace("  ", "")
-    print("company: ", company)
-
-    location=s.div.contents[7].contents[5].text
-    location=location.replace("\n", "").replace("  ", "")
-    print("location: ",location)
-
-    exp=s.div.contents[9].contents[3].text
-    exp=exp.replace("\n","").replace("  ","")
-    print("exp: ",exp)
-
-    tags=s.div.contents[9].contents[5].text
-    tags=tags.replace("\n", "").replace("  ", "")
-    print("tags: ",tags)
-
-    jobType=s.div.contents[9].contents[1].text
-    jobType=jobType.replace("\n", "").replace("  ", "")
-    print("job Type: ",jobType)
-
-    time=s.div.find("time").attrs['title']
-    time=time.replace("\n", "").replace("  ", "")
-    print("time: ",time)
+    job.getJobDynamic(s)
+    print("title: ",job.title)
+    print("company: ", job.company)
+    print("location: ", job.location)
+    print("exp: ", job.exp)
+    print("expLevel: ", job.expLevel)
+    print("tags: ", job.tags)
+    print("job Type: ", job.jobType)
+    print("post time: ", job.postTime)
 
     print("\n\n\n")
-
