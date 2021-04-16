847393974be49524a1b4698b4017e330b7c4063c
Ahmed Fathy <ahmedfatmahmoud@gmail.com>
2021-04-16 13:09:44 +0200

    edit new file paths

--- clock.py
+++ clock.py
@@ -6,11 +6,11 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=30)
 def timed_job():
-    os.system("python3 './src/wuzzuf/wuzzufTelegramJobee'")
-    os.system("python3 './src/linkedIn/linkedinTelegramJobee'")
+    os.system("python3 './src/wuzzuf/wuzzufTelegramJobee.py'")
+    os.system("python3 './src/linkedIn/linkedinTelegramJobee.py'")
 
 
-@sched.scheduled_job('cron', day_of_week='fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='tue', hour=17)
 def scheduled_job():
     os.system("python3 './src/wuzzuf/weekly_backup.py'")
 
